c0d18e06e3eaced8efa3eac3dc0758256628363c
Patrik Nilsson <craftopia@live.com>
2017-10-01 19:05:13 +0200

    revers

--- src/led-start.c
+++ src/led-start.c
@@ -129,7 +129,7 @@ int main( int argc, char *argv[] )
             {
                 float percentag = i/100.0f;
                 for (int i = 0; i < CHANNELS; i++) {
-                    DMX_Data[i+1] = DMX_Orginal_Data[i+1] + (BYTE)((float)(DMX_Orginal_Data[i+1] - DMX_Target_Data[i+1]) * percentag);
+                    DMX_Data[i+1] = DMX_Orginal_Data[i+1] - (BYTE)((float)(DMX_Orginal_Data[i+1] - DMX_Target_Data[i+1]) * percentag);
                 }
 
                 //Send Dmx
