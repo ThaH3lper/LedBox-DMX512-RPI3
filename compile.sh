44eee19a356826b58e277ca72aa9c29847ac97de
Patrik Nilsson <craftopia@live.com>
2018-05-11 14:28:14 +0200

    moved and changed compiler script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-gcc -o build/dmx512 src/dmx512.c -L. -lftd2xx -Wl,-rpath /usr/local/lib
-gcc -o build/led-start src/led-start.c -L. -lftd2xx -Wl,-rpath /usr/local/lib
-gcc -o build/led src/led.c
-sudo cp ./build/led /usr/bin/
-sudo cp ./build/led-start /usr/bin/
+
+#gcc -o LedLib/build/dmx512 src/dmx512.c -L. -lftd2xx -Wl,-rpath /usr/local/lib
+
+#gcc -o build/led-start src/led-start.c -L. -lftd2xx -Wl,-rpath /usr/local/lib
+
+
+printf "\033[1;30mBuilding: Led\n"
+
+g++ LedLib/src/Led.cpp LedLib/src/Dmx/DmxController.cpp LedLib/src/SharedMemory/SharedMemory.cpp LedLib/src/Server.cpp LedLib/src/Behavior/BaseBehavior.hpp LedLib/src/Behavior/SinCurve.hpp LedLib/src/Behavior/FadeToSimple.hpp LedLib/src/global.cpp -o LedLib/build/Led -std=gnu++11 -L. -lftd2xx -Wl,-rpath /usr/local/lib
+
+
+#sudo cp ./build/led /usr/bin/
+
+#sudo cp ./build/led-start /usr/bin/
+
+
+printf "\033[0;32m[Builds Completed]\n"
+sudo cp .LedLib/build/Led /usr/bin/
+printf "\033[0;32m[File copied]\n"
