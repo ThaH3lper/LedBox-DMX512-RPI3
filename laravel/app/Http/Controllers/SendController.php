752a66f30441d357e0ea7da93c10ced31cca971b
Patrik Nilsson <craftopia@live.com>
2018-05-11 15:16:07 +0200

    fixed hex to int

--- laravel/app/Http/Controllers/SendController.php
+++ laravel/app/Http/Controllers/SendController.php
@@ -11,6 +11,11 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class SendController extends Controller
 {
+    private function hexToInt($hex)
+    {
+        return max(0, min(127, floor(hexdec($hex) / 2)));
+    }
+
     public function welcome(Request $request)
     {
         return view('welcome');
@@ -19,15 +24,21 @@ class SendController extends Controller
     public function allsend(Request $request)
     {
     	$color = $request->input('color');
-    	$process = new Process('led ' . substr($color, 0, 2) . ' ' . substr($color, 2, 2) . ' ' . substr($color, 4, 2));
+        $red = max(0, min(127, floor(hexdec(substr($color, 0, 2)) / 2)));
+        $green = max(0, min(127, floor(hexdec(substr($color, 2, 2)) / 2)));
+        $blue = max(0, min(127, floor(hexdec(substr($color, 4, 2)) / 2)));
+    	$process = new Process('Led all 1 ' . $red . ' ' . $green . ' ' . $blue);
 		$process->run();
-        return view('allsend', ['name' => $color]);
+        return view('allsend', ['name' => 'Led all 1 ' . $red . ' ' . $green . ' ' . $blue]);
     }
 
     public function sliderpicker(Request $request)
     {
         $color = $request->input('color');
-        $process = new Process('led ' . substr($color, 0, 2) . ' ' . substr($color, 2, 2) . ' ' . substr($color, 4, 2));
+        $red = max(0, min(127, floor(hexdec(substr($color, 0, 2)) / 2)));
+        $green = max(0, min(127, floor(hexdec(substr($color, 2, 2)) / 2)));
+        $blue = max(0, min(127, floor(hexdec(substr($color, 4, 2)) / 2)));
+        $process = new Process('Led all 1  ' . $red . ' ' . $green . ' ' . $blue);
         $process->run();
         return view('sliderpicker', ['color_hex' => $color]);
     }
@@ -36,7 +47,10 @@ class SendController extends Controller
     {
         $channel = $request->input('channel');
         $color = $request->input('color');
-        $process = new Process('led ' . $channel . ' ' . substr($color, 0, 2) . ' ' . substr($color, 2, 2) . ' ' . substr($color, 4, 2));
+        $red = max(0, min(127, floor(hexdec(substr($color, 0, 2)) / 2)));
+        $green = max(0, min(127, floor(hexdec(substr($color, 2, 2)) / 2)));
+        $blue = max(0, min(127, floor(hexdec(substr($color, 4, 2)) / 2)));
+        $process = new Process('Led channel ' . $channel . ' 1 ' . $red . ' ' . $green . ' ' . $blue);
         $process->run();
         return view('singlesend', ['color' => $color]);
     }
